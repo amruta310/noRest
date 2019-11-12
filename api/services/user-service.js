84c27bebb58ee9471603f9616c52993d14100935
Alisha Mehta <mehta.al@husky.neu.edu>
2019-11-12 18:35:05 -0500

    Added server.js and user-service.js

---
+++ api/services/user-service.js
@@ -0,0 +1,61 @@
+/**
+ * Service for noRest operations.
+ */
+
+'use strict';
+/** 
+ * require mongoose
+ */
+const mongoose = require('mongoose'),
+    Users = mongoose.model('Users');
+
+/**
+ * Returns an array of User objects.
+ *
+ * @param {Object} params {Search parameters}
+ */
+exports.search = function (params) {
+    const promise = Users.find(params).exec();
+    return promise;
+};
+
+/**
+ * Saves and returns the new user object.
+ *
+ * @param {Object} User {User object}
+ */
+exports.save = function (user) {
+    const newUser = new Users(user);
+    const promise = newUser.save();
+    return promise;
+};
+
+/**
+ * Returns the user object matching the id.
+ *
+ * @param {string} userId {Id of the user object}
+ */
+exports.get = function (userId) {
+    const promise = Users.findById(userId).exec();
+    return promise;
+};
+
+/**
+ * Updates and returns the user object.
+ *
+ * @param {Object} user {User object}
+ */
+exports.update = function (user) {
+    const promise = Users.findOneAndUpdate({_id: user._id}, user).exec();
+    return promise;
+};
+
+/**
+ * Deletes the user object matching the id.
+ *
+ * @param {string} userId {Id of the user object}
+ */
+exports.delete = function (userId) {
+    const promise = Users.remove({_id: userId});
+    return promise;
+};
