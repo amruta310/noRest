d6c43c981be403428cacb34f89cf05a641eecb57
Alisha Mehta <mehta.al@husky.neu.edu>
2019-11-12 18:04:28 -0500

    Created DB for User, Event and Animal

---
+++ api/models/eventSchema.js
@@ -0,0 +1,43 @@
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/**
+ * schema maps to a MongoDB collection and defines the shape of the documents within that collection.
+ */
+let eventSchema = new Schema({
+    title: {
+        type: String,
+        required: "name is required"
+    },
+    description: {
+        type: String,
+        required: "description is required"
+    },
+    location: {
+        type: String,
+        required: "location is required"
+    },
+    date: {
+        type: Date,
+        required: "date is required"
+    },
+    amount: {
+        type: Decimal128,
+        required: "amount is required"
+    }
+}, {
+    versionKey: false
+});
+
+// Duplicate the id field as mongoose returns _id field instead of id.
+eventSchema.virtual('id').get(function(){
+    return this._id.toHexString();
+});
+
+// Ensure virtual fields are serialised.
+eventSchema.set('toJSON', {
+    virtuals: true
+});
+
+module.exports = mongoose.model('Events', eventSchema);
---
+++ api/models/userSchema.js
@@ -0,0 +1,39 @@
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/**
+ * schema maps to a MongoDB collection and defines the shape of the documents within that collection.
+ */
+let userSchema = new Schema({
+    name: {
+        type: String,
+        required: "name is required"
+    },
+    username: {
+        type: String,
+        required: "username is required"
+    },
+    password: {
+        type: String,
+        required: "password is required"
+    },
+    type: {
+        type: String,
+        required: "type is required"
+    }
+}, {
+    versionKey: false
+});
+
+// Duplicate the id field as mongoose returns _id field instead of id.
+userSchema.virtual('id').get(function(){
+    return this._id.toHexString();
+});
+
+// Ensure virtual fields are serialised.
+userSchema.set('toJSON', {
+    virtuals: true
+});
+
+module.exports = mongoose.model('Users', userSchema);
