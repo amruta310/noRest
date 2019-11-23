d2de5eb28517a743e5500681e22b1256850db49e
Alisha Mehta <mehta.al@husky.neu.edu>
2019-11-23 12:35:07 -0500

    Created logo and header component

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { EventListComponent } from './event-list/event-list.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardViewComponent },
+  // { path: '', component: DashboardViewComponent },
   { path: 'events', component: EventListComponent },
 ];
 
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { MaterialModule } from './material.module';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 import { BrowserModule, DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -5,17 +7,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardViewComponent } from './dashboard-view/dashboard-view.component';
 import { EventListComponent } from './event-list/event-list.component';
-
+import { LogoComponent } from './logo/logo.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
     DashboardViewComponent,
-    EventListComponent
+    EventListComponent,
+    LogoComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
