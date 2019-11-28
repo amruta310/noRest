85d5ba45573fb1778a4f4140bdd17a6541e6cfcf
Utkarsha <mahajan.u@husky.neu.edu>
2019-11-28 14:12:41 -0500

    SideBar added

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -6,14 +6,20 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { DonationComponent } from './donation/donation.component';
 import { AnimalComponent } from './animal/animal.component';
 import { AnimalEachComponent } from './animal/animal-each/animal-each.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   //{ path: '', component: DashboardViewComponent },
   { path: 'events', component: EventListComponent },
   { path: 'donation', component: DonationComponent },
-  { path:'animal', component:AnimalComponent },
+  { path: 'animal', component:AnimalComponent },
   { path: 'signUpOrLogin/:param', component: SignUpComponent },
-  { path:'animaleach', component:AnimalEachComponent }
+  { path: 'animaleach', component:AnimalEachComponent },
+  { path: 'aboutUs', component:AboutUsComponent},
+  { path: 'contactUs', component:ContactUsComponent},
+  { path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
