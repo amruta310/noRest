b447c70f79e3303ee1909ad10bff8b00b4819a68
amruta310 <patil.amr@husky.neu.edu>
2019-12-10 16:09:34 -0500

    ask question backend

---
+++ Backend/api/models/askQuestion.js
@@ -0,0 +1,27 @@
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/**
+ * schema maps to a MongoDB collection and defines the shape of the documents within that collection.
+ */
+let askQuestionSchema = new Schema({
+    question: {
+        type: String,
+        required: "question is required"
+    }
+}, {
+    versionKey: false
+});
+
+// Duplicate the id field as mongoose returns _id field instead of id.
+askQuestionSchema.virtual('id').get(function(){
+    return this._id.toHexString();
+});
+
+// Ensure virtual fields are serialised.
+askQuestionSchema.set('toJSON', {
+    virtuals: true
+});
+
+module.exports = mongoose.model('Askquestion', askQuestionSchema);
